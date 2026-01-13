0a12219edea0403038f7701cfb49f32095d809a2
Mitya <skie_link@mail.ru>
2026-01-13 15:13:47 +0300

    add comments

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -14,38 +14,38 @@ public class Main {
 
         while (i) { // цикл
             System.out.print("Введите знак операции: ");
-            String operation = scanner.next();
+            String operation = scanner.next(); // ввод операции
 
-            if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")) {
-                System.out.println(operation);
+            if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")) { // проверка на ввод именно операции
+                System.out.println(operation); // вывод операции
 
                 System.out.println("Введите первое число: ");
-                double a = scanner.nextDouble();
+                double a = scanner.nextDouble(); // ввод первого числа
 
                 System.out.println("Введите второе число: ");
-                double b = scanner.nextDouble();
+                double b = scanner.nextDouble(); // ввод второго числа
 
-                switch (operation) {
+                switch (operation) { // выбор действий для каждого оператора
                     case "+": {
-                        System.out.println("Сумма чисел: " + (a + b));
+                        System.out.println("Сумма чисел: " + (a + b)); // сумма
                         break;
                     }
                     case "-": {
-                        System.out.println("Разность чисел: " + (a - b));
+                        System.out.println("Разность чисел: " + (a - b)); // разность
                         break;
                     }
                     case "*": {
-                        System.out.println("Произведение чисел: " + (a * b));
+                        System.out.println("Произведение чисел: " + (a * b)); // произведение
                         break;
                     }
                     case "/": {
-                        System.out.println("Частное чисел: " + (a / b));
+                        System.out.println("Частное чисел: " + (a / b)); // частное
                         break;
                     }
                 }
                 System.out.println("Выйти? (да/нет)");
-                String exit = scanner.next();
-                if (exit.equals("да")) {
+                String exit = scanner.next(); // ввод намерения выйти или остаться
+                if (exit.equals("да")) { // проверка ввода
                     i = false;
                 }
             } else break;
